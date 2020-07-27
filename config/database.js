5d31acfa7d20a7c7cda381f9c8b938195d3bc750
muflihfq <31898437+muflihfq@users.noreply.github.com>
2020-07-27 14:00:21 +0700

    fix pathname

--- config/database.js
+++ config/database.js
@@ -56,7 +56,7 @@ module.exports = {
             port: Env.get('DB_PORT', ''),
             user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
             password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-            database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname)
+            database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substring(1))
         }
     },
 
